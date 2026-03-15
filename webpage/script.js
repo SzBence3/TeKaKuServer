948bf8165ff33538258f07a22bc403df9055ca69
SzBence <szbenedek0407@gmail.com>
2026-03-15 16:24:25 +0100

    Added tutorial page

--- webpage/script.js
+++ webpage/script.js
@@ -5,6 +5,8 @@ function main(){
     navbardiv.innerHTML = '\
         <a href="index.html">Főoldal</a> | \
         <a href="about.html">A projektről</a> | \
+        <a href="tutorial.html">Útmutató</a> | \
+        <a href="gyik.html">GYIK</a> | \
         <a href="legal-notice.html">Jogi nyilatkozat</a>\
         ' ;
     navbardiv.classList.add("navbar");
